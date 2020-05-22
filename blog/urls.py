214f45ebc11e237a1f336af675bd7efac194749a
David Wachtel <david.wachtel@outlook.com>
2020-05-22 12:10:09 -0700

    blog integration

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path, include
+from blog import views
+
+
+urlpatterns = [
+    path('', views.allblogs, name='allblogs'),
+    path('<int:blog_id>/', views.blogDetail, name='blogDetail'),
+]
